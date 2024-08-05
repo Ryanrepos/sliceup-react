19752d36b3bdbf8d97154c18062da6d3484226f8
ryanrepos <mamarasulov911@gmail.com>
2024-08-05 15:55:57 +0900

    fix: soved some bugs

--- src/app/screens/homePage/ActiveUsers.tsx
+++ src/app/screens/homePage/ActiveUsers.tsx
@@ -3,14 +3,20 @@ import React from 'react';
 import { Box, Container, Stack } from "@mui/material";
 import { CssVarsProvider } from '@mui/joy';
 
-const activeUsers = [
-    {memberNick: "Martin", memberImage: "/img/martin.webp" },
-    {memberNick: "Justin", memberImage: "/img/justin.webp" },
-    {memberNick: "Rose", memberImage: "/img/rose.webp" },
-    {memberNick: "Nusret", memberImage: "/img/nusret.webp" },
-];
+import { useSelector } from "react-redux";
+import { createSelector } from "reselect";
+import { retrieveTopUsers } from "./selector";
+import { serverApi } from '../../lib/config';
+import { Member } from '../../lib/types/member';
 
+/** REDUX SLICE & SELECTOR **/
+  
+  const topUsersRetriever = createSelector (
+    retrieveTopUsers, 
+    (topUsers) => ({topUsers})
+  );
 export default function ActiveUsers() {
+    const {topUsers} = useSelector(topUsersRetriever);
     return (
     <div className={"active-users-frame"}>
         <Container>
@@ -18,12 +24,14 @@ export default function ActiveUsers() {
                 <Box className={"category-title"}>Active Users</Box>
                 <Stack className={"cards-frame"}>
                     <CssVarsProvider>
-                        {activeUsers.length !== 0 ? (
-                            activeUsers.map((ele, index) => {
+                        {topUsers.length !== 0 ? (
+                            topUsers.map((member: Member) => {
+                                const imagePath = `${serverApi}/${member.memberImage}`
+
                                 return (
                                     <Stack className={"card"}>
-                                        <img src={ele.memberImage} />
-                                        <div className={"card-name"}>{ele.memberNick}</div>
+                                        <img src={imagePath} />
+                                        <div className={"card-name"}>{member.memberNick}</div>
                                     </Stack>
                                 );
                             }) ) : (<Box className={"no-data"}> No Active Users!</Box>)}
